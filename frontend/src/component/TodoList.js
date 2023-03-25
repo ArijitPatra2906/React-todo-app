8af3b6f9b6b6e4e4d77eee2cd124e8aac306a58a
Arijit Patra <patraarijit440@gmail.com>
2023-03-25 18:27:17 +0530

    responsive

--- frontend/src/component/TodoList.js
+++ frontend/src/component/TodoList.js
@@ -25,7 +25,7 @@ const TodoList = ({ todos, setTodos, setEditTodo }) => {
                 <li className='list-item' key={todo.id}>
                     <input type="text" value={todo.title} className={`list ${todo.completed ? "complete" : ""}`} onChange={(e) => e.preventDefault()} />
 
-                    <div>
+                    <div className='list_buttons'>
                         <button className='button-complete task-button' onClick={() => handleComplete(todo)}>
                             <i className='fa fa-check-circle'></i>
                         </button>
